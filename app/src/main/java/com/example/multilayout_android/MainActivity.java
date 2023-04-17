1c28d14942a8260f4216d546170a558615963379
baekmingyu <bbg999123@gmail.com>
2023-04-17 16:47:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/multilayout_android/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.multilayout_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.BreakIterator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
